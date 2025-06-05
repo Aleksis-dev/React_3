079f2c35a25f959c5f0c6e2f316559e185382301
CrazyIvanovs <lastovskis08@gmail.com>
2025-06-05 15:59:29 +0300

    Added form component

--- src/App.jsx
+++ src/App.jsx
@@ -10,6 +10,20 @@ function savePost({post}) {
   localStorage.setItem("posts", JSON.stringify(storedArray));
 }
 
+function Form({submitHandler}) {
+  return (
+    <>
+      <form onSubmit={submitHandler}>
+        <label htmlFor="title">Title:</label> <br />
+        <input type="text" name="title" placeholder="Your amazing title" /> <br />
+        <label htmlFor="content">Content:</label> <br />
+        <textarea name="content" placeholder="My extordinary post content"></textarea> <br />
+        <button>Post</button>
+      </form>
+    </>
+  )
+}
+
 function App() {
   return (
     <>
